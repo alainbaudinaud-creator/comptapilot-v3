98fdeb9c488d3190d17c1a95f6a086b469d0f996
Alain <ton@email.com>
2026-05-22 17:41:54 +0200

    Correction conflit database avec db_core

--- db_core/connection.py
+++ db_core/connection.py
@@ -1,6 +1,6 @@
 ﻿import sqlite3
 
-from database.settings import SQLITE_DB
+from db_core.settings import SQLITE_DB
 
 def get_sqlite_connection():
     return sqlite3.connect(SQLITE_DB)
--- repositories/pdp_v3/supervision_repository.py
+++ repositories/pdp_v3/supervision_repository.py
@@ -1,4 +1,4 @@
-﻿from database.connection import get_sqlite_connection
+﻿from db_core.connection import get_sqlite_connection
 
 def fetch_supervision_stats():
 
