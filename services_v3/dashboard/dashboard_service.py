57b50274a82239bc9c5157979d74bc1fdad4400c
Alain <ton@email.com>
2026-05-23 17:50:34 +0200

    Ajout couche services V3 dashboard supervision

---
+++ services_v3/dashboard/dashboard_service.py
@@ -0,0 +1,20 @@
+﻿from repositories.ecritures.dashboard_repository import fetch_global_stats
+
+
+def get_dashboard_financier():
+
+    stats = fetch_global_stats()
+
+    total_credit = stats["total_credit"]
+    total_debit = stats["total_debit"]
+
+    resultat = round(total_credit - total_debit, 2)
+
+    return {
+        "ca": round(total_credit, 2),
+        "charges": round(total_debit, 2),
+        "resultat": resultat,
+        "tva_estimee": round(total_credit * 0.20, 2),
+        "nb_ecritures": stats["nb_ecritures"],
+        "statut": "ok"
+    }
---
+++ services_v3/supervision/supervision_service.py
@@ -0,0 +1,14 @@
+﻿from repositories.ecritures.supervision_repository import fetch_nombre_ecritures
+from repositories.ecritures.supervision_repository import fetch_totaux_debit_credit
+
+
+def get_supervision_comptable():
+
+    totaux = fetch_totaux_debit_credit()
+
+    return {
+        "nb_ecritures": fetch_nombre_ecritures(),
+        "total_debit": totaux["total_debit"],
+        "total_credit": totaux["total_credit"],
+        "ecart": round(totaux["total_debit"] - totaux["total_credit"], 2)
+    }
