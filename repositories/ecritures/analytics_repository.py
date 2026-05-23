d53e5fcae84df1f41dbf6cad841cf1824f2a72fa
Alain <ton@email.com>
2026-05-23 16:51:38 +0200

    Ajout repository analytics écritures V3

---
+++ repositories/ecritures/analytics_repository.py
@@ -0,0 +1,6 @@
+﻿from repositories.ecritures.stats_repository import (
+    fetch_global_stats,
+    fetch_totaux_debit_credit,
+    fetch_dashboard_financier_par_type,
+    fetch_nombre_ecritures
+)
