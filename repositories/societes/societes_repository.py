010593c9c8fef9291d4b8543edc3b30148d70dc2
Alain <ton@email.com>
2026-05-23 17:28:49 +0200

    Ajout repository societes PostgreSQL V3

---
+++ repositories/societes/societes_repository.py
@@ -0,0 +1,32 @@
+﻿from sqlalchemy import text
+
+from database import engine
+
+
+def fetch_societes():
+
+    with engine.begin() as con:
+
+        result = con.execute(text("""
+            SELECT *
+            FROM societes
+            ORDER BY id
+        """))
+
+        return result.fetchall()
+
+
+def fetch_societe_by_id(societe_id: int):
+
+    with engine.begin() as con:
+
+        result = con.execute(text("""
+            SELECT *
+            FROM societes
+            WHERE id = :societe_id
+            LIMIT 1
+        """), {
+            "societe_id": societe_id
+        })
+
+        return result.fetchone()
