cc5196529896a2ea3f344387fe163b5796d1cd7e
Alain <ton@email.com>
2026-05-23 16:44:16 +0200

    Ajout repository dashboard écritures V3

---
+++ repositories/ecritures/dashboard_repository.py
@@ -0,0 +1,6 @@
+﻿from repositories.ecritures.stats_repository import (
+    fetch_global_stats,
+    fetch_totaux_debit_credit,
+    fetch_nombre_ecritures,
+    fetch_dashboard_financier_par_type
+)
