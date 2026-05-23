a9958898d5a3e0d84e29a4cafcf6cd716ff3bffc
Alain <ton@email.com>
2026-05-24 01:01:19 +0200

    Ajout schemas réponses API V3

---
+++ schemas_v3/api_response.py
@@ -0,0 +1,19 @@
+﻿def success_response(data=None, message="ok"):
+
+    return {
+        "success": True,
+        "message": message,
+        "data": data or {}
+    }
+
+
+def error_response(message="error", code="UNKNOWN_ERROR", details=None):
+
+    return {
+        "success": False,
+        "message": message,
+        "error": {
+            "code": code,
+            "details": details or {}
+        }
+    }
