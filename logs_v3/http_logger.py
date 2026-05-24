9f1ec7762d4b5aaa435e5ebf1de8ec6d5b926553
Alain <ton@email.com>
2026-05-24 10:31:34 +0200

    Ajout logger HTTP V3

---
+++ logs_v3/http_logger.py
@@ -0,0 +1,19 @@
+﻿from flask import request
+
+from logs_v3.logger import log_info
+
+
+def log_http_response(response):
+
+    log_info(
+        "http",
+        "Requête HTTP",
+        {
+            "method": request.method,
+            "path": request.path,
+            "status_code": response.status_code,
+            "remote_addr": request.remote_addr
+        }
+    )
+
+    return response
