d99908aac4c0a084ad7591dee96b186f03966eae
Alain BAUDINAUD <alain.baudinaud@orange.fr>
2026-05-28 23:21:09 +0000

    Migre bilan et compte de resultat vers PostgreSQL

--- controllers/bilan_auto.py
+++ controllers/bilan_auto.py
@@ -1,24 +1,30 @@
-﻿from flask import Blueprint, render_template
-import sqlite3
+from flask import Blueprint, render_template
+from sqlalchemy import text
+from database import engine
 
 bp_bilan = Blueprint("bilan_auto", __name__)
 
 @bp_bilan.route("/bilan")
 def bilan():
+
     rows = []
 
     try:
-        con = sqlite3.connect("db.sqlite")
-        cur = con.cursor()
-        rows = cur.execute("""
-            SELECT *
-            FROM vue_bilan
-        """).fetchall()
-        con.close()
+        with engine.begin() as conn:
+            rows = conn.execute(text("""
+                SELECT
+                    numero,
+                    libelle,
+                    type,
+                    solde
+                FROM vue_bilan_postgres
+                ORDER BY numero
+            """)).fetchall()
+
     except Exception as e:
         print("BILAN WARNING:", e)
-        rows = []
 
-    return render_template("comptabilite/bilan.html", rows=rows)
-
-
+    return render_template(
+        "comptabilite/bilan.html",
+        rows=rows
+    )
