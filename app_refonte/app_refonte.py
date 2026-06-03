d042b6c083411279ab56170ea6ba32d658a7dcc4
Alain BAUDINAUD <alain.baudinaud@orange.fr>
2026-06-03 09:59:05 +0000

    Ajoute centre revision cabinet premium

--- app_refonte/app_refonte.py
+++ app_refonte/app_refonte.py
@@ -142,7 +142,11 @@ def create_app_refonte():
     return app
 
 
-if __name__ == "__main__":
+
+@app.route("/revision-cabinet")
+def revision_cabinet():
+    return render_template("revision_cabinet.html")
+\n\nif __name__ == "__main__":
     app = create_app_refonte()
     app.run(host="127.0.0.1", port=5099, debug=True)
 
